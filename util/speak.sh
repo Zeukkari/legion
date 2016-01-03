de21eb69c032c20ee1fe58c0d8d9b0500fbfaeb9
Timo Aho <zeukkari@gmail.com>
2016-01-03 08:02:22 +0200

    Local Festival TTS

--- util/speak.sh
+++ util/speak.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "$@" | text2wave -o /tmp/result.wav && castnow /tmp/result.wav --device VoiceOfLegion && rm /tmp/result.wav
+#echo "$@" | text2wave -o /tmp/result.wav && castnow /tmp/result.wav --device VoiceOfLegion && rm /tmp/result.wav
+echo "(SayText \""$@"\") ">/tmp/legionspeech | festival --pipe "/tmp/legionspeech"
